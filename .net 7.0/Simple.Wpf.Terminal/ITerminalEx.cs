930227be7a14d0d31983da882ff259316144e3ae
Adrian Krol <130042081+adrian-krol@users.noreply.github.com>
2023-05-22 06:50:08 +0900

    Update ITerminalEx.cs

--- .net 7.0/Simple.Wpf.Terminal/ITerminalEx.cs
+++ .net 7.0/Simple.Wpf.Terminal/ITerminalEx.cs
@@ -6,18 +6,12 @@ namespace Simple.Wpf.Terminal;
 
 public interface ITerminalEx : ITerminal
 {
-    /// <summary>
     ///     The error color for the bound items.
-    /// </summary>
     IValueConverter LineColorConverter { get; set; }
 
-    /// <summary>
     ///     The individual line height for the bound items.
-    /// </summary>
     int ItemHeight { get; set; }
 
-    /// <summary>
     ///     The margin around the bound items.
-    /// </summary>
     Thickness ItemsMargin { get; set; }
 }
